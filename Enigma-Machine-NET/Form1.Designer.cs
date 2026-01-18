545fbe7eb6bb4a103501400f5ba1f5a230206eca
Mihai <seleamihai@gmail.com>
2026-01-18 07:25:05 -0800

    Change Main form title to "Enigma Machine" Spawn Form 1 in the center screen

--- Enigma-Machine-NET/Form1.Designer.cs
+++ Enigma-Machine-NET/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Enigma Machine";
+            ResumeLayout(false);
         }
 
         #endregion
