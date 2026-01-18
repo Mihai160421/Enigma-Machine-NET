6b5fab03cd6f10cd753cc7b84c94b5431cab2bd4
Mihai <seleamihai@gmail.com>
2026-01-18 08:37:15 -0800

    Manually place buttons Add keyboard textbox

--- Enigma-Machine-NET/Form1.Designer.cs
+++ Enigma-Machine-NET/Form1.Designer.cs
@@ -28,19 +28,369 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button10 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            button21 = new Button();
+            button22 = new Button();
+            button23 = new Button();
+            button24 = new Button();
+            button25 = new Button();
+            button26 = new Button();
+            keyboard_textbox = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(123, 322);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 160);
+            button1.TabIndex = 0;
+            button1.Text = "Q";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += TastaApasata_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(289, 322);
+            button2.Name = "button2";
+            button2.Size = new Size(160, 160);
+            button2.TabIndex = 1;
+            button2.Text = "W";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += TastaApasata_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(455, 322);
+            button3.Name = "button3";
+            button3.Size = new Size(160, 160);
+            button3.TabIndex = 2;
+            button3.Text = "E";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += TastaApasata_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(953, 322);
+            button4.Name = "button4";
+            button4.Size = new Size(160, 160);
+            button4.TabIndex = 5;
+            button4.Text = "Z";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += TastaApasata_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(787, 322);
+            button5.Name = "button5";
+            button5.Size = new Size(160, 160);
+            button5.TabIndex = 4;
+            button5.Text = "T";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += TastaApasata_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(621, 322);
+            button6.Name = "button6";
+            button6.Size = new Size(160, 160);
+            button6.TabIndex = 3;
+            button6.Text = "R";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += TastaApasata_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(1451, 322);
+            button7.Name = "button7";
+            button7.Size = new Size(160, 160);
+            button7.TabIndex = 8;
+            button7.Text = "O";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += TastaApasata_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(1285, 322);
+            button8.Name = "button8";
+            button8.Size = new Size(160, 160);
+            button8.TabIndex = 7;
+            button8.Text = "I";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += TastaApasata_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(1119, 322);
+            button9.Name = "button9";
+            button9.Size = new Size(160, 160);
+            button9.TabIndex = 6;
+            button9.Text = "U";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += TastaApasata_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(1369, 502);
+            button11.Name = "button11";
+            button11.Size = new Size(160, 160);
+            button11.TabIndex = 16;
+            button11.Text = "K";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += TastaApasata_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(1203, 502);
+            button12.Name = "button12";
+            button12.Size = new Size(160, 160);
+            button12.TabIndex = 15;
+            button12.Text = "J";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += TastaApasata_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(1037, 502);
+            button13.Name = "button13";
+            button13.Size = new Size(160, 160);
+            button13.TabIndex = 14;
+            button13.Text = "H";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += TastaApasata_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(871, 502);
+            button14.Name = "button14";
+            button14.Size = new Size(160, 160);
+            button14.TabIndex = 13;
+            button14.Text = "G";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += TastaApasata_Click;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(705, 502);
+            button15.Name = "button15";
+            button15.Size = new Size(160, 160);
+            button15.TabIndex = 12;
+            button15.Text = "F";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += TastaApasata_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(539, 502);
+            button16.Name = "button16";
+            button16.Size = new Size(160, 160);
+            button16.TabIndex = 11;
+            button16.Text = "D";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += TastaApasata_Click;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(373, 502);
+            button17.Name = "button17";
+            button17.Size = new Size(160, 160);
+            button17.TabIndex = 10;
+            button17.Text = "S";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += TastaApasata_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(207, 502);
+            button18.Name = "button18";
+            button18.Size = new Size(160, 160);
+            button18.TabIndex = 9;
+            button18.Text = "A";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += TastaApasata_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(1468, 678);
+            button10.Name = "button10";
+            button10.Size = new Size(160, 160);
+            button10.TabIndex = 25;
+            button10.Text = "L";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += TastaApasata_Click;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(1302, 678);
+            button19.Name = "button19";
+            button19.Size = new Size(160, 160);
+            button19.TabIndex = 24;
+            button19.Text = "M";
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += TastaApasata_Click;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(1136, 678);
+            button20.Name = "button20";
+            button20.Size = new Size(160, 160);
+            button20.TabIndex = 23;
+            button20.Text = "N";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += TastaApasata_Click;
+            // 
+            // button21
+            // 
+            button21.Location = new Point(970, 678);
+            button21.Name = "button21";
+            button21.Size = new Size(160, 160);
+            button21.TabIndex = 22;
+            button21.Text = "B";
+            button21.UseVisualStyleBackColor = true;
+            button21.Click += TastaApasata_Click;
+            // 
+            // button22
+            // 
+            button22.Location = new Point(804, 678);
+            button22.Name = "button22";
+            button22.Size = new Size(160, 160);
+            button22.TabIndex = 21;
+            button22.Text = "V";
+            button22.UseVisualStyleBackColor = true;
+            button22.Click += TastaApasata_Click;
+            // 
+            // button23
+            // 
+            button23.Location = new Point(638, 678);
+            button23.Name = "button23";
+            button23.Size = new Size(160, 160);
+            button23.TabIndex = 20;
+            button23.Text = "C";
+            button23.UseVisualStyleBackColor = true;
+            button23.Click += TastaApasata_Click;
+            // 
+            // button24
+            // 
+            button24.Location = new Point(472, 678);
+            button24.Name = "button24";
+            button24.Size = new Size(160, 160);
+            button24.TabIndex = 19;
+            button24.Text = "X";
+            button24.UseVisualStyleBackColor = true;
+            button24.Click += TastaApasata_Click;
+            // 
+            // button25
+            // 
+            button25.Location = new Point(306, 678);
+            button25.Name = "button25";
+            button25.Size = new Size(160, 160);
+            button25.TabIndex = 18;
+            button25.Text = "Y";
+            button25.UseVisualStyleBackColor = true;
+            button25.Click += TastaApasata_Click;
+            // 
+            // button26
+            // 
+            button26.Location = new Point(140, 678);
+            button26.Name = "button26";
+            button26.Size = new Size(160, 160);
+            button26.TabIndex = 17;
+            button26.Text = "P";
+            button26.UseVisualStyleBackColor = true;
+            button26.Click += TastaApasata_Click;
+            // 
+            // keyboard_textbox
+            // 
+            keyboard_textbox.Location = new Point(140, 869);
+            keyboard_textbox.Name = "keyboard_textbox";
+            keyboard_textbox.Size = new Size(1471, 47);
+            keyboard_textbox.TabIndex = 26;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1698, 1257);
+            ClientSize = new Size(2671, 2221);
+            Controls.Add(keyboard_textbox);
+            Controls.Add(button10);
+            Controls.Add(button19);
+            Controls.Add(button20);
+            Controls.Add(button21);
+            Controls.Add(button22);
+            Controls.Add(button23);
+            Controls.Add(button24);
+            Controls.Add(button25);
+            Controls.Add(button26);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button17);
+            Controls.Add(button18);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Enigma Machine";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button10;
+        private Button button19;
+        private Button button20;
+        private Button button21;
+        private Button button22;
+        private Button button23;
+        private Button button24;
+        private Button button25;
+        private Button button26;
+        private TextBox keyboard_textbox;
     }
 }
--- Enigma-Machine-NET/Form1.cs
+++ Enigma-Machine-NET/Form1.cs
@@ -1,50 +1,20 @@
-﻿namespace Enigma_Machine_NET
+﻿using Microsoft.VisualBasic.Devices;
+
+namespace Enigma_Machine_NET
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-
-            InitializeKeyboard();
-        }
-
-        private void InitializeKeyboard()
-        {
-            // Keyboard Layout
-            string[] rows = {   
-                "QWERTZUIOP", 
-                "ASDFGHJKL", 
-                "PYXCVBNM" 
-            };
-
-            for (int r = 0; r < rows.Length; r++)
-            {
-                for (int i = 0; i < rows[r].Length; i++)
-                {
-                    const int _bsize = 200;
-                    const int _padding = 5;
-
-                    Button btn = new Button();
-
-                    btn.Text = rows[r][i].ToString();
-                    btn.Width = _bsize;
-                    btn.Height = _bsize;
-                    
-                    btn.Left = 50 + (i * _bsize+ _padding) + (r * 20);
-                    btn.Top = 100 + (r * _bsize+ _padding);
-
-                    btn.Click += TastaApasata_Click;
-
-                    this.Controls.Add(btn);
-                }
-            }
         }
 
         private void TastaApasata_Click(object? sender, EventArgs e)
         {
             Button btn = (Button)sender!;
             char litera = btn.Text[0];
+
+            this.keyboard_textbox.Text += litera ;
         }
     }
 }
